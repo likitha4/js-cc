565dbf58fe372fdea42bb9d811ab60c83fa22c31
likitha4 <likithagollapalli2001@gmail.com>
2024-02-20 15:25:28 +0530

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,29 @@
+"use strict" //use code as new js (classes, arrow func)
+
+// alert(4-8) we are using node js not browser
+//code readability
+
+console.log(8 - 8);
+console.log("name")
+
+let age=896989
+let name="john"
+let state=null;
+let isLoggedIn=false
+
+console.log(typeof(isLoggedIn));
+console.log(typeof(age));
+console.log(typeof(name));
+console.log(typeof state);
+console.log(typeof null); //object
+console.log(typeof undefined) //undefined
+
+
+//types--> number--2 to power 53,
+//  bigint, string, boolean,
+//  null-- standalone
+// undefined
+// symbol
+// object
+
+
