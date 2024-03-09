4189a01e9325757afc3172b88a09ac35ff2a0adf
likitha4 <likithagollapalli2001@gmail.com>
2024-03-09 15:18:32 +0530

    new

---
+++ loops/for.js
@@ -0,0 +1,41 @@
+// array=[1,2,3,4,56,78,90]
+// for (let index = 0; index < array.length; index++) {
+//     // const element = array[index];
+//     console.log(`${array[index]} is the element`);
+// }
+// for(let i=0;i<3;i++)
+// {
+//     console.log(`outer loop ${i}`);
+//     for(let j=0;j<3;j++)
+//     {
+//         console.log(`inner loop ${j}`);
+//     }
+// }
+// for(let a=0;a<34;a++)
+// {
+//     if(a==7 || a==14 || a==21){
+//     continue;
+//     }
+// console.log(a," is the element");
+// }
+
+for(let a=0;a<34;a++)
+{
+    if(a==7 || a==14 || a==21){
+    break;
+    }
+console.log(a," is the element");
+}
+
+array=["oranges","grapes","banana"]
+// for (let index = 0; index <=array.length; index++) {
+//     const element = array[index];
+//     console.log(element);
+    
+//undefined is the last element in the above result 
+
+// for (let index = 0; index <array.length; index++) {
+//         const element = array[index];
+//         console.log(element);
+        
+//     }
---
+++ loops/while.js
@@ -0,0 +1,19 @@
+let index=0;
+while(index<10)
+{
+    console.log(`${index} is the number`);
+    index=index+3;
+}
+let arr=["manisha","hanisha","joe","racheal"]
+let ar=0
+while(ar<arr.length)
+{
+    console.log(`${arr[ar]} is element`);
+    ar++;
+}
+
+let score=10
+do{
+console.log(`${score} is this for now`);
+score--;
+}while(score>=10);
